0934c5a4a3629284ae7eb80e852488a734e07474
catonrug <aigars.kadikis@gmail.com>
2016-03-02 20:19:48 +0200

    test

--- check.sh
+++ check.sh
@@ -6,7 +6,7 @@
 #sudo apt-get update -y && sudo apt-get upgrade -y
 #sudo apt-get install git -y
 #mkdir -p /home/pi/detect && cd /home/pi/detect
-#git clone https://github.com/catonrug/teamviewer-detect.git && cd teamviewer-detect && chmod +x check.sh && ./check.sh
+#git clone https://github.com/catonrug/salidzini.git && cd salidzini && chmod +x check.sh && ./check.sh
 
 #check if script is located in /home direcotry
 pwd | grep "^/home/" > /dev/null
